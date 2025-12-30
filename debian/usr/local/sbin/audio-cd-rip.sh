1bad718666aff62e621f1af1d1d236fd40efe9b8
Matt Renyard <renyard.m@gmail.com>
2025-12-30 20:31:09 +0000

    More generic install scripts

--- debian/usr/local/sbin/audio-cd-rip.sh
+++ debian/usr/local/sbin/audio-cd-rip.sh
@@ -3,9 +3,9 @@
 LOGFILE=/var/log/rip.log;
 echo -e "\nAudio CD RIP CALLED: $(date)" >> $LOGFILE;
 abcde -Nx 2>&1 >> $LOGFILE;
-if [ -d "/home/mrenyard/Music/Unknown_Artist/Unknown_Album" ]; then
-  UA_COUNT=`find /home/mrenyard/Music/Unknown_Artist/* -maxdepth 1 -type d | wc -l`;
-  mv /home/mrenyard/Music/Unknown_Artist/Unknown_Album /home/mrenyard/Music/Unknown_Artist/Album_${UA_COUNT};
+if [ -d "[HOME]/Music/Unknown_Artist/Unknown_Album" ]; then
+  UA_COUNT=`find [HOME]/Music/Unknown_Artist/* -maxdepth 1 -type d | wc -l`;
+  mv [HOME]/Music/Unknown_Artist/Unknown_Album [HOME]/Music/Unknown_Artist/Album_${UA_COUNT};
   echo -e "\nSaved as: Album_${UA_COUNT}" >> $LOGFILE;
 fi
 exit 0;
--- debian/usr/local/sbin/movie-dvd-rip.sh
+++ debian/usr/local/sbin/movie-dvd-rip.sh
@@ -4,15 +4,15 @@ LOGFILE=/var/log/rip.log
 echo -e "\nMovie DVD RIP CALLED: $(date)" >> $LOGFILE;
 TITLE=$(blkid -o value -s LABEL /dev/sr1);
 echo -e "DVD Title: $TITLE" >> $LOGFILE;
-HandBrakeCLI --input "/dev/sr1" --title "${TITLE}" --main-feature --output "/home/mrenyard/Movies/${TITLE}.mp4";
+HandBrakeCLI --input "/dev/sr1" --title "${TITLE}" --main-feature --output "[HOME]/Movies/${TITLE}.mp4";
 rc=$?
 if [[ $rc != 0 ]] ; then
   echo "FAILED!" >> $LOGFILE;
   eject /dev/sr1;
   exit $rc;
 fi
-sudo chmod +x "/home/mrenyard/Movies/${TITLE}.mp4";
-sudo chown mrenyard:mrenyard "/home/mrenyard/Movies/${TITLE}.mp4";
+sudo chmod +x "[HOME]/Movies/${TITLE}.mp4";
+sudo chown [USER]:[USER] "[HOME]/Movies/${TITLE}.mp4";
 echo "done!" >> $LOGFILE;
 eject /dev/sr1;
 exit 0;
